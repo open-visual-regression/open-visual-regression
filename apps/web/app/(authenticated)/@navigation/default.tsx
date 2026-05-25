a6fd1b1a5ddf9fc19b3038ff068f57fef056dbf3
Tom Fischer <tgfischer6@gmail.com>
2026-05-25 09:48:31 -0400

    (feat) use the logo in the navigation bar

--- apps/web/app/(authenticated)/@navigation/default.tsx
+++ apps/web/app/(authenticated)/@navigation/default.tsx
@@ -1,5 +1,10 @@
+import { Logo } from "../../../lib/components/logo/Logo";
 import { NavigationBar } from "../../../lib/components/navigation-bar/NavigationBar";
 
 export default function NavigationPage() {
-  return <NavigationBar>Hello, World!</NavigationBar>;
+  return (
+    <NavigationBar>
+      <Logo size="sm" />
+    </NavigationBar>
+  );
 }
